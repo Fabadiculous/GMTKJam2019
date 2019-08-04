ed3c1f18fdfb4d8da755270bf10b994c7de6bbae
Fabadiculous <24710205+Fabadiculous@users.noreply.github.com>
2019-08-04 16:29:27 +1000

    Game config

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import 'phaser'
+
+//Scenes
+import Menu from './scenes/Menu'
+import Play from './scenes/Play'
+
+const config = {
+    type: Phaser.AUTO,
+    backgroundColor: '#00BFFF',
+    physics: {
+        default: 'arcade'
+    },
+    scale: {
+        parent: 'game-div',
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    title: "GMTKJam 2019 Only Once",
+    scene: [Menu, Play]
+}
+
+let game = new Phaser.Game(config)
